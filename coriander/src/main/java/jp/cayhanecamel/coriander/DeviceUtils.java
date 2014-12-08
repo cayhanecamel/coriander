687f4a6b326f241ab070b66d64cc386e045c88ea
cayhanecamel <cayhane.camel@gmail.com>
2014-12-08 15:19:56 +0900

    init

---
+++ coriander/src/main/java/jp/cayhanecamel/coriander/DeviceUtils.java
@@ -0,0 +1,12 @@
+package jp.cayhanecamel.coriander;
+
+/**
+ * Created by annaka.suguru on 2014/12/08.
+ */
+public class DeviceUtils {
+
+
+    public static String test() {
+        return "hoge";
+    }
+}
